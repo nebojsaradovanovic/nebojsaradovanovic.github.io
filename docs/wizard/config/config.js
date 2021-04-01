c882950e103f5e0acb052f85e74598e955848552
nebojsaradovanovic <nebojsa.radovanovic@startelecom.ca>
2021-04-01 20:50:52 +0200

    new

--- docs/wizard/config/config.js
+++ docs/wizard/config/config.js
@@ -1,12 +1,12 @@
 export default {
-    clientID: 'd7501314-e8af-40f5-ab32-228bc6259736',
+    clientID: 'e7de8a75-62bb-43eb-9063-38509f8c21af',
 
     // 'wizardUriBase': 'http://localhost:8080/wizard/',
-    wizardUriBase: 'https://nebojsaradovanovic.github.io/docs/wizard/',
+    wizardUriBase: 'https://mypurecloud.github.io/purecloud-premium-app/wizard/',
 
     // The actual URL of the landing page of your web app.
     // 'premiumAppURL': 'http://localhost:8080/premium-app-sample/index.html',
-    premiumAppURL: 'https://nebojsaradovanovic.github.io/docs/premium-app-sample/index.html',
+    premiumAppURL: 'https://mypurecloud.github.io/purecloud-premium-app/premium-app-sample/index.html',
 
     // Genesys Cloud assigned name for the premium app
     // This should match the integration type name of the Premium App
